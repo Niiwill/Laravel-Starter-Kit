d17d10441799db7a2f65ca71bf31460e549ea421
Niiwill <Niiwill94@gmail.com>
2025-10-21 20:29:55 +0200

    Add files via upload

---
+++ routes/api.php
@@ -0,0 +1,13 @@
+<?php
+
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Route;
+
+Route::get('/user', function (Request $request) {
+    return $request->user();
+})->middleware('auth:sanctum');
+
+// Index route for API
+Route::get('/', function () {
+    return response()->json(['message' => 'Welcome to the API!']);
+});
